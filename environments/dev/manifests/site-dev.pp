200416aba7e1a58b67fc62e8b07919f2fe8d1eb1
Mike Renfro <mike+github@renf.ro>
2012-03-21 14:20:16 -0500

    Adding RSA-keyed login from gold server to client

--- environments/dev/manifests/site-dev.pp
+++ environments/dev/manifests/site-dev.pp
@@ -1,1 +1,10 @@
 node default {}
+
+node gold {
+  include ssh
+}
+
+node scratchy {
+  include ssh
+  include root_rsa_id
+}
---
+++ environments/dev/modules/root_rsa_id/manifests/init.pp
@@ -0,0 +1,15 @@
+class root_rsa_id {
+  file { "/root/.ssh":
+    ensure => directory,
+    mode   => 0700,
+    owner  => "root",
+    group  => "root";
+  }
+  file { "/root/.ssh/authorized_keys":
+    source  => "puppet:///modules/${module_name}/id_rsa.pub",
+    mode    => 0600,
+    owner   => "root",
+    group   => "root",
+    require => File["/root/.ssh"];
+  }
+}
